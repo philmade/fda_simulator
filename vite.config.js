dd891aadb6a4a66c245561b1cba5a1f6c0f8642a
philmade <46587871+philmade@users.noreply.github.com>
2026-01-06 12:18:22 +0000

    buikld fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'build'
+  build: {
+    outDir: 'build', // Forces Vite to output to 'build' instead of 'dist'
+  }
 })
